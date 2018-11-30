45f1072652db792663de789d73abcbe56ba0ebf5
rocco <r.righi@elsevier.com>
2018-11-30 10:42:00 +0000

    Fix getCoinInfo

--- src/fetchers.js
+++ src/fetchers.js
@@ -1,19 +1,46 @@
 import rp from 'request-promise-native';
 
+const vault = {
+  BTC: 0.553,
+  XRP: 0.488,
+  ETH: 0.234,
+  BCH: 0.528,
+  XLM: 0.075,
+  EOS: 0.946,
+  LTC: 0.344,
+  USDT: 0.89,
+  BSV: 0.462,
+  ADA: 0.883,
+  TRX: 0.396,
+  XMR: 0.025,
+  MIOTA: 0.7,
+  DASH: 0.07,
+  XEM: 0.623,
+  BNB: 0.886,
+  NEO: 0.324,
+  ETC: 0.238,
+  ZEC: 0.048,
+  BTG: 0.501,
+};
+
+
 const promiseCoins = () => {
   const options = {
     uri: 'https://chasing-coins.com/api/v1/top-coins/20',
     json: true,
   }
-  return rp(options).then(obj => Object.values(obj));
+  return rp(options)
+    .then(coinsObj => Object.values(coinsObj)
+      .map(coin => ({ ...coin, amount: vault[coin.symbol] }))
+    );
 };
 
-const getCoinInfo = (symbol) => {
+const getCoinInfo = ({ symbol, amount }) => {
   const options = {
     uri: `https://chasing-coins.com/api/v1/std/coin/${symbol}`,
     json: true,
   }
-  return rp(options)
+  return rp(options).then(coinInfo => ({ ...coinInfo, symbol, amount }));
 }
 
 const getCoinValue = (symbol, amount) => {
@@ -21,9 +48,7 @@ const getCoinValue = (symbol, amount) => {
     uri: `https://chasing-coins.com/api/v1/convert/${symbol}/EUR`,
     json: true,
   }
-  return rp(options).then((result) => {
-    parseInt(result) * amount
-  });
+  return rp(options).then(({ result }) => parseInt(result) * amount);
 }
 
 export { promiseCoins, getCoinInfo, getCoinValue };
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -4,29 +4,6 @@ import {
   getCoinInfo
 } from './fetchers';
 
-const vault = {
-  BTC: 0.55354996,
-  XRP: 0.4880728391,
-  ETH: 0.2341394760,
-  BCH: 0.5286716764,
-  XLM: 0.0758702173,
-  EOS: 0.9464430211,
-  LTC: 0.3445457100,
-  USDT: 0.899024804,
-  BSV: 0.4624739863,
-  ADA: 0.8833145466,
-  TRX: 0.3962778871,
-  XMR: 0.0255185535,
-  MIOTA: 0.76895031,
-  DASH: 0.073539604,
-  XEM: 0.6239409655,
-  BNB: 0.8865389592,
-  NEO: 0.3248120400,
-  ETC: 0.2381571768,
-  ZEC: 0.0488353336,
-  BTG: 0.5016146754,
-};
-
 const typeDefs = `
 type Query {
   coins: [Coin]
@@ -34,7 +11,7 @@ type Query {
 type CoinInfo {
   price: String
   coinheat: Int
-  amountInPounds: Int
+  amountInPounds: Float
 }
 type Coin {
   amount: Float
@@ -50,14 +27,9 @@ const resolvers = {
     coins: () => promiseCoins(),
   },
   Coin: {
-    amount: ({symbol}) => vault[symbol],
-    symbol: ({symbol}) => symbol,
-    cap: ({cap}) => cap,
-    coinInfo: ({symbol}) => getCoinInfo(symbol)
+    coinInfo: (parent) => getCoinInfo(parent)
   },
   CoinInfo: {
-    price: ({price}) => price,
-    coinheat: ({coinheat}) => coinheat,
     amountInPounds: ({ symbol, amount }) => getCoinValue(symbol, amount)
   }
 }
